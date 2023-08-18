87703f9c9071fca67c4b81ea1efc9ccf5cecac14
Reidmen <r.rethmawn@gmail.com>
2023-08-18 22:34:05 +0200

    added more methods to the book header with implementation

--- include/book.cpp
+++ include/book.cpp
@@ -16,6 +16,64 @@ void Book::end_order_deferral() {
     }
 }
 
+void Book::execute_bid(ConstOrderPtr &order) {
+    auto limit_iteration = asks.begin();
+    double order_price = order->price;
+
+    while (limit_iteration != asks.end() &&
+           limit_iteration->first <= order_price && order->quantity > 0.0) {
+        if (limit_iteration->second.trade(order) > 0.0) {
+            market_price = limit_iteration->first;
+        }
+
+        if (limit_iteration->second.is_empty()) {
+            asks.erase(limit_iteration++);
+        } else {
+            ++limit_iteration;
+        }
+    }
+
+    auto trigger_limit_iteration = ask_triggers.begin();
+
+    while (trigger_limit_iteration != ask_triggers.end() &&
+           trigger_limit_iteration->first <= market_price) {
+        trigger_limit_iteration->second.trigger_all();
+        ++trigger_limit_iteration;
+    }
+
+    ask_triggers.erase(ask_triggers.begin(), trigger_limit_iteration);
+}
+
+
+void Book::queue_bid_order(ConstOrderPtr &order) {
+    const auto limit_iteration = bids.emplace(order->price, OrderLimit()).first;
+    const auto order_iteration = limit_iteration->second.insert(order);
+
+    order->limit_iteration = limit_iteration;
+    order->order_iteration = order_iteration;
+    order->queued = true;
+    check_asks_all_or_nothing(order->price);
+    order->on_queue();
+
+}
+
+void Book::insert_all_or_nothing_bid(ConstOrderPtr &order) {
+    if (bid_is_fillable(order)) {
+        execute_bid(order);
+        order->book = nullptr;
+        return;
+    }
+
+    if (order->immediate_or_cancel) {
+        order->on_canceled();
+        order->book = nullptr;
+        return;
+    }
+
+    // queue unexecuted in all-or-nothing order
+    queue_bid_order(order);
+}
+
 void Book::insert_bid(ConstOrderPtr &order) {
     execute_bid(order);
 
--- include/book.hpp
+++ include/book.hpp
@@ -63,6 +63,61 @@ class Book {
     inline void insert_all_or_nothing_bid(ConstOrderPtr &order);
     inline void insert_all_or_nothing_ask(ConstOrderPtr &order);
 
+    /*
+     * @brief check if the bid order can be filled completely.
+     * This check is performec before all-or-nothing order
+     * are executed.
+     *
+     * @param order, the all-or-nothing bid order to be executed
+     * @return true if the order is completely fillable
+     * @return false if the order is partially fillable
+     */
+    inline bool bid_is_fillable(ConstOrderPtr order) const;
+
+    /*
+     * @brief check if the ask order can be filled completely.
+     * This check is performec before all-or-nothing order
+     * are executed.
+     *
+     * @param order, the all-or-nothing ask order to be executed
+     * @return true if the order is completely fillable
+     * @return false if the order is partially fillable
+     */
+    inline bool ask_is_fillable(ConstOrderPtr order) const;
+
+    inline void execute_bid(ConstOrderPtr &order);
+    inline void execute_ask(ConstOrderPtr &order);
+
+    inline void execute_queued_bid(ConstOrderPtr &order);
+    inline void execute_queued_ask(ConstOrderPtr &order);
+
+    inline void queue_bid_order(ConstOrderPtr &order);
+    inline void queue_ask_order(ConstOrderPtr &order);
+
+    inline void queue_bid_trigger(ConstTriggerPtr trigger);
+    inline void queue_ask_trigger(ConstTriggerPtr trigger);
+
+    /*
+     * @brief check if any all-or-nothing bids at the specified
+     * price or lower are executable. This function is called
+     * if the quantity of queued order is increased.
+     *
+     * @param price, the price from which queued all-or-nothing
+     * will be checked.
+     */
+    inline void check_bids_all_or_nothing(const double price);
+
+    /*
+     * @brief check if any all-or-nothing asks at the specified
+     * price or ask are executable. This function is called
+     * if the quantity of queued order is increased.
+     *
+     * @param price, the price from which queued all-or-nothing
+     * will be checked.
+     */
+    inline void check_asks_all_or_nothing(const double price);
+
+
    public:
     template <class T, class... Args>
     inline std::shared_ptr<T> insert(Args &&... args);
